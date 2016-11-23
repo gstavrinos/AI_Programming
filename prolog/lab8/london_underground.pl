ead9618409cc32d017a938ed1de8150af15f3bbf
George Stavrinos <gstavrinos@iit.demokritos.gr>
2016-11-23 22:15:25 +0200

    Lab8 is NOW ready. I added a reverse function, in order for the final result to be in the expected order!

--- prolog/lab8/london_underground.pl
+++ prolog/lab8/london_underground.pl
@@ -14,8 +14,11 @@ line(tottenhamCourtRoad,oxfordCircus).
 line(oxfordCircus,warrenStreet).
 line(warrenStreet,goodgeStreet).
 
-/* Helper function for both questions b and c */
+/* Helper functions for both questions b and c */
 edge(Station1,Station2):-line(Station1,Station2);line(Station2,Station1).
+start_rev(Path,RevPath):-rev(Path,[],RevPath).
+rev([H|T],Remainder,RevPath):-rev(T,[H|Remainder],RevPath).
+rev([],Remainder,Remainder). 
 
 /* question b */
 
@@ -23,13 +26,12 @@ goal(kingsCrossStPancras).
 
 findroute(S):-dfs_qb([],oxfordCircus,S).
 
-dfs_qb(Path,Curr,[kingsCrossStPancras|Path]):-goal(Curr),!.
+dfs_qb(Path,Curr,S):-goal(Curr),!,start_rev([kingsCrossStPancras|Path],S).
 dfs_qb(Path,Curr,S):-edge(Curr,Next),not(member(Curr,Path)),dfs_qb([Curr|Path],Next,S).
-
 
 /* question c */
 
 findroute(Init,Goal,S):-dfs_qc([],Init,Goal,S).
 
-dfs_qc(Path, Curr, Goal, [Goal|Path]):-Curr=Goal,!.
-dfs_qc(Path, Curr, Goal, S):-edge(Curr,Next),not(member(Curr,Path)),dfs_qc([Curr|Path], Next, Goal, S).
+dfs_qc(Path,Curr,Goal,S):-Curr=Goal,!,start_rev([Goal|Path],S).
+dfs_qc(Path,Curr,Goal,S):-edge(Curr,Next),not(member(Curr,Path)),dfs_qc([Curr|Path], Next, Goal, S).
